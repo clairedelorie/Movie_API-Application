7133b82602eaf64d38dd87f1137598c48f9e8387
clairedelorie <82280517+clairedelorie@users.noreply.github.com>
2021-07-28 22:14:02 -0400

    added files

---
+++ models.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+let movieSchema = mongoose.Schema({
+    Title: {type: String, required: true},
+    Description: {type: String, required: true},
+    Genre: {
+        Name: String,
+        Description: String
+      },
+      Director: {
+        Name: String,
+        Bio: String
+      },
+    ImageUrl: String,
+});
+
+let userSchema = mongoose.Schema({
+  Username: {type: String, required: true},
+  Password: {type: String, required: true},
+  Email: {type: String, required: true},
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+const User = mongoose.model('User', userSchema);
+
+
+module.exports.Movie = Movie;
+module.exports.User = User;
