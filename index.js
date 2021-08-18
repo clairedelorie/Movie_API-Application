75cb173c47f051f8a0013988b190cf7cb408eb3f
clairedelorie <82280517+clairedelorie@users.noreply.github.com>
2021-08-18 19:18:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get("/", (req, res) => {
 //get all movies
 app.get(
   "/movies",
-//   passport.authenticate("jwt", { session: false }),
+  passport.authenticate("jwt", { session: false }),
   (req, res) => {
     Movies.find()
       .then((movies) => {
