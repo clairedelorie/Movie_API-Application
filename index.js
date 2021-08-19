1c39b3bf341e0390f44b49732ca6b8d8bae17927
Claire De Lorie <delorie.claire@gmail.com>
2021-08-18 22:29:35 -0400

    Update index.js
    changed app.get/movies passport.authenticate for lesson

--- index.js
+++ index.js
@@ -32,20 +32,16 @@ app.get("/", (req, res) => {
 });
 
 //get all movies
-app.get(
-  "/movies",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    Movies.find()
-      .then((movies) => {
-        res.status(201).json(movies);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error: " + err);
-      });
-  }
-);
+app.get("/movies", (req, res) => {
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
 
 //Get a movie by title
 app.get(
