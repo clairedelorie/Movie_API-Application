5b313312e363a422176fcbfeee2bf87da9250d6c
Claire De Lorie <delorie.claire@gmail.com>
2021-08-18 21:46:09 -0400

    Update index.js
    deleted old mongoose.connect code

--- index.js
+++ index.js
@@ -12,11 +12,6 @@ const Users = Models.User;
 const Genres = Models.Genre;
 const Directors = Models.Director;
 
-// mongoose.connect("mongodb://localhost:27017/myFlixDB", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
 mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
